b3a9bdc42b31298585c7f06b614e7eb21e98677e
devikaps <devikanup@gmail.com>
2024-10-07 07:46:12 +1100

    Kurskal Implementation

--- generator/kruskalGenerator.py
+++ generator/kruskalGenerator.py
@@ -17,6 +17,12 @@ class KruskalMazeGenerator():
     TODO: Complete the implementation (Task A)
     """
 
+    # Defined two classes
+    #     - UnionFind() - implements union-find datastructure
+    #     - Edge()  - to represent a node in the min-span-tree heap
+    #     - build_wall_heap - to keep the walls and its weight in heap
+    #                         as heap serves the purpose of retrieving the smallest wall
+
     class UnionFind():
         """Disjoint set class to be used for implemeting union-find datastructure
 
@@ -38,18 +44,16 @@ class KruskalMazeGenerator():
         def __init__(self, vertices: Coordinates) -> None:
             """Step 1:"""
             self.parent = dict()
-            self.level = dict()
 
             for vertex in vertices:
                 # set key as itsemf
                 self.parent.update({vertex: vertex})
-                self.level.update({vertex: 0})
 
 
         def find(self, vertex):
             """Step #2."""
             if self.parent[vertex] != vertex:
-                return self.parent[vertex]
+                return self.find(self.parent[vertex])
 
             # if parent is the vertex (inital case)
             return vertex
@@ -59,17 +63,11 @@ class KruskalMazeGenerator():
             parent1 = self.find(vertex1)
             parent2 = self.find(vertex2)
 
-            if parent1 == parent2: return
+            if parent1 == parent2: return False
 
-            # Setting the new parennt
-            if self.level[parent1] < self.level[parent2]:
-                self.parent[parent1] = parent2
-            elif self.level[parent1] > self.level[parent2]:
-                self.parent[parent2] = parent1
-            else:
-                self.parent[parent2] =  parent1
-                # with new child added, increse the level of the parent
-                self.level[parent1] += 1
+            # Setting the new parent
+            self.parent[parent1] = parent2
+            return True
 
     class Edge:
         """Edge class to define a node to be used in heap
@@ -77,12 +75,26 @@ class KruskalMazeGenerator():
                 - edge => maze.edge
                 - weigth => maze.edgeWeight
         """
-        def __init__(self, edge, weight:int) -> None:
-            self.edge = edge
+        def __init__(self, node, weight:int) -> None:
+            self.node = node
             self.weight = weight
 
-        def __lt__(self, edge):
-            return self.weight < edge.weight
+        def __lt__(self, node):
+            return self.weight < node.weight
+
+
+    def build_wall_heap(self, maze) ->list():
+        """Building heap with edges and their weights"""
+        edges = list()
+        heapq.heapify(edges)
+
+        for edge in maze.getEdges():
+            if maze.hasWall(edge[0], edge[1]):
+                distance = maze.edgeWeight(edge[0],edge[1])
+                node = self.Edge(edge,distance)
+                heapq.heappush(edges, node)
+
+        return edges
 
     def generateMaze(self, maze:Maze):
         """
@@ -91,54 +103,40 @@ class KruskalMazeGenerator():
         PseudoCode:
             1. Fetch the max iterations (No.Of vertieces-1)
             2. Keep the edges in a heapq.
-               This will help in reducing the complexity.
-            3. for each edge.
-            4.     fetch the edge with lowest cost.
+               This will help in reducing th complexity.
+            3. for each wall.
+            4.     fetch the wall with lowest cost towards entrance.
             5.     add to the spanning tree if no cycle forms.
-
+            6.     remove the wall
         Datastructures Used:
             - min-heap
             - union-find
 
         Time complexity:
-            V(log E) -> for V - vertices & E - Edges
+            V(log W) -> for V - vertices & W - Walls
 
         """
         # TODO: Implement this method for task A.
 
         # step 1: Fetch the max iterations (No.Of vertieces-1)
-        iterations = len(maze.getCoords()) - 1
+        total_vertices = len(maze.getCoords())
         min_span_tree = list()
 
-        # step 2: Keep the edges in a heapq
-        edges = list()
-        heapq.heapify(edges)
-        for edge in maze.getEdges():
-            node = self.Edge(edge,maze.edgeWeight(edge[0],edge[1]))
-            heapq.heappush(edges, node)
+        # print(maze.getEntrances())
+        # step 2: Keep the edges in a heapq with entrances
+        walls = self.build_wall_heap(maze)
 
         # step 3
         unionFind = self.UnionFind(maze.getCoords())
-        while iterations > 0 and edges:
+        while len(min_span_tree) < total_vertices - 1 and walls:
             # step 4
-            smallest_edge = heapq.heappop(edges)
-            # print(f"Choosing edge {smallest_edge}")
+            curr_wall = heapq.heappop(walls)
+            selected_wall = curr_wall.node
+            vertex1 = selected_wall[0]
+            vertex2 = selected_wall[1]
 
-            selected_edge = smallest_edge.edge
-            vertex1 = selected_edge[0]
-            vertex2 = selected_edge[1]
-
-            # step 5
-            # expand min-span_tree if the new edge will not form a cycle
-            if unionFind.find(vertex1) != unionFind.find(vertex2):
-                # merge the parents to find the cycle later
-                unionFind.union(unionFind.find(vertex1),
-                                unionFind.find(vertex2))
-
-                min_span_tree.append(selected_edge)
-
-                # remove wall if it exists
+            # checking if removing the wall will merge two disjoint sets
+            if unionFind.union(vertex1, vertex2):
+                min_span_tree.append(selected_wall)
+                # remove wall if it is the shortest path towards exits
                 maze.removeWall(vertex1,vertex2)
-
-            # Mark completion for one iteration
-            iterations -= 1
